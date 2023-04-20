d81a8c269e931f71f2a5f44094198a41632af38d
Emiel de Heij <emieldeheij@hotmail.com>
2023-04-20 11:24:17 +0200

    authenticate with more user control

--- src/utils.py
+++ src/utils.py
@@ -4,7 +4,7 @@ import json
 
 import spotipy
 from google.cloud import secretmanager
-from spotipy.oauth2 import SpotifyClientCredentials
+from spotipy.oauth2 import CacheHandler, SpotifyOAuth
 
 
 def extract_json_from_response(response: str) -> str:
@@ -26,8 +26,17 @@ class SpotifyClient:
     def __init__(self) -> None:
         cid = self.access_secret_version("projects/420207002838/secrets/SPOTIFY_CLIENT_ID/versions/1")
         secret = self.access_secret_version("projects/420207002838/secrets/SPOTIFY_CLIENT_SECRET/versions/1")
-        client_credentials_manager = SpotifyClientCredentials(client_id=cid, client_secret=secret)
-        self.spotify = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
+        redirect_uri = self.access_secret_version("projects/420207002838/secrets/SPOTIFY_REDIRECT_URI/versions/latest")
+
+        self.spotify = spotipy.Spotify(
+            auth_manager=SpotifyOAuth(
+                client_id=cid,
+                client_secret=secret,
+                redirect_uri=redirect_uri,
+                cache_handler=GoogleCacheHandler(),
+                scope="playlist-modify-public",
+            )
+        )
 
     @staticmethod
     def access_secret_version(secret_version_id: str) -> str:
@@ -44,3 +53,21 @@ class SpotifyClient:
         response = client.access_secret_version(name=secret_version_id)
 
         return response.payload.data.decode("UTF-8")
+
+
+class GoogleCacheHandler(CacheHandler):
+    """Class with custom implementation of Spotipy's CacheHandler.
+
+    This implementation loads and saves the cached access tokens to GCP.
+    """
+
+    def get_cached_token(self) -> dict:
+        """Get and return a token_info dictionary object."""
+        return json.loads(
+            SpotifyClient.access_secret_version("projects/420207002838/secrets/DOT-CACHE/versions/latest")
+        )
+
+    def save_token_to_cache(self, token_info: dict) -> None:
+        """Save a token_info dictionary object to the cache and return None."""
+        # TO-DO: update dot-cache with refreshed token
+        return None
