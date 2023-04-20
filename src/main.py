75daac7122828d4ff4c1aec51453a4547800d08f
Emiel de Heij <emieldeheij@hotmail.com>
2023-04-20 16:23:26 +0200

    temporary solution to hardcode title

---
+++ src/main.py
@@ -0,0 +1,24 @@
+"""This is the main module to generate a themed playlist on a weekly basis, piecing other modules together."""
+
+import fastapi
+
+from playlist_generator import compose_playlist
+from playlist_publisher import publish_playlist_to_spotify
+from utils import get_logger
+
+main = fastapi.FastAPI()
+
+logger = get_logger(__name__)
+
+
+@main.post("/weekly_themed_playlist")
+def create_themed_playlist(theme: str) -> str:
+    """Create a themed playlist on a weekly basis.
+
+    Returns:
+        str: A message informing the user that the playlist has been successfully updated.
+    """
+    playlist = compose_playlist(theme=theme)
+    playlist["title"] = "GPTunes test playlist"  # tmp
+    logger.info(f"Sending playlist titled '{playlist['title']}' to publisher-service.")
+    return publish_playlist_to_spotify(playlist)
