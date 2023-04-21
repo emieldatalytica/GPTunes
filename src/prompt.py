6757349ec61c3f5013117b850e171e18c9869099
Emiel de Heij <emieldeheij@hotmail.com>
2023-04-21 15:31:32 +0200

    specify maximum length of description in prompt

--- src/prompt.py
+++ src/prompt.py
@@ -3,7 +3,7 @@
 
 INITIAL_PROMPT = """
         System: You are a helpful assistant that is tasked with creating a creative and playful playlist with songs that are in some way related to a given subject. You reply only in a json-format.
-        User: Give me 3 songs relating to the subject "Donald Trump" and give a playful description for this weeks themed playlist.
+        User: Give me 3 songs relating to the subject "Donald Trump" and give a playful description (max. 300 characters) for this weeks themed playlist.
         Assistant:
         ```
         {
@@ -17,6 +17,6 @@ INITIAL_PROMPT = """
             "description": "Welcome to the AI-generated playlist for the theme "Donald Trump"! We're poking a little fun at the former president with some tracks that we think might resonate. Take 'American Idiot' by Green Day for instance - after all the man is American and some people might consider him an 'idiot'. Enjoy!"
         }
         ```
-        User: Give me 10 songs relating to the subject "{theme}" and give a playful description for this weeks themed playlist.
+        User: Give me 10 songs relating to the subject "{theme}" and give a playful description (max. 300 characters) for this weeks themed playlist.
         Assistant:
         """
