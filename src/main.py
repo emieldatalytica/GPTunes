9be712c90c93749e0d4bff25fa18267cf9a5cd9a
Emiel de Heij <emieldeheij@hotmail.com>
2023-05-11 16:51:34 +0200

    add redirect to fastapi docs

--- src/main.py
+++ src/main.py
@@ -1,6 +1,7 @@
 """This is the main module to generate a themed playlist on a weekly basis, piecing other modules together."""
 
 import fastapi
+from fastapi.responses import RedirectResponse
 
 from playlist_generator import compose_playlist
 from playlist_publisher import publish_playlist_to_spotify
@@ -9,6 +10,11 @@ from utils import get_logger
 main = fastapi.FastAPI()
 
 logger = get_logger(__name__)
+
+
+@main.get("/", include_in_schema=False)
+def docs_redirect() -> RedirectResponse:
+    return RedirectResponse("/docs")
 
 
 @main.post("/create_themed_playlist")
