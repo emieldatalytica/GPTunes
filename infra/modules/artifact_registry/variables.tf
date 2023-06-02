9d9ee88d89650d38e8d2e8baecf719cb021d22dc
Emiel de Heij <emieldeheij@hotmail.com>
2023-06-02 10:28:50 +0200

    clean up infra outcommented stuff

--- infra/modules/artifact_registry/variables.tf
+++ infra/modules/artifact_registry/variables.tf
@@ -1,10 +1,3 @@
-
-# variable "gcp_project" {
-#   description = "GCP project"
-#   type        = string
-#   default     = "gptunes-dev"
-# }
-
 variable "gcp_service_account" {
   description = "GCP service account"
   type        = string
