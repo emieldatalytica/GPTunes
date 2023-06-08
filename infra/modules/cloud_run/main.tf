3f9bd728b78cbad3fd94999404a8d17a6ef821e8
Emiel de Heij <emieldeheij@hotmail.com>
2023-06-08 12:14:05 +0200

    try v1 of cloud run resources

--- infra/modules/cloud_run/main.tf
+++ infra/modules/cloud_run/main.tf
@@ -1,23 +1,26 @@
-resource "google_cloud_run_v2_service_iam_member" "member" {
-  project = google_cloud_run_v2_service.default.project
-  location = google_cloud_run_v2_service.default.location
-  name = google_cloud_run_v2_service.default.name
+resource "google_cloud_run_service_iam_member" "member" {
+  project = google_cloud_run_service.default.project
+  location = google_cloud_run_service.default.location
+  service = google_cloud_run_service.default.name
   role = "roles/run.admin"
   member = "serviceAccount:${var.gcp_service_account}"
 }
 
-resource "google_cloud_run_v2_service" "default" {
+resource "google_cloud_run_service" "default" {
   name     = "gptunes-api"
   location = "europe-west4"
-  ingress = "INGRESS_TRAFFIC_ALL"
 
   template {
+    spec {
+      service_account_name = var.gcp_service_account
       containers {
-        name = "gptunes-backend"
-        ports {
-            container_port = 8080
-        }
         image = "europe-west4-docker.pkg.dev/gptunes-dev/gptunes-backend/gptunes-backend:${var.image_tag}"
       }
+    }
+  }
+
+  traffic {
+    percent         = 100
+    latest_revision = true
   }
 }
